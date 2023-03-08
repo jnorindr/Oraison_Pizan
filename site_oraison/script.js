9faa90dba0569c1f7dd55af37ff0cde8ffc5f2d9
jnorindr <jade.norindr@gmail.com>
2023-03-08 14:16:48 +0100

    Ajout de commentaires dans script.js

--- site_oraison/script.js
+++ site_oraison/script.js
@@ -1,7 +1,9 @@
 // Bloc déroulable de la bibliographie
+// Déclaration des variables
 var coll = document.getElementsByClassName("biblio");
 var i;
 
+// Animation en cas de click sur le bloc
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
@@ -17,10 +19,12 @@ for (i = 0; i < coll.length; i++) {
 //Bouton de retour en haut de la page
 let mybutton = document.getElementById("bouton_retour");
 
+// Quand l'utilisateur descend sur la page, activation de la fonction
 window.onscroll = function () {
 scrollFunction();
 };
 
+// Fonction pour l'affichage du bouton quand l'utilisateur descend à plus de 20 px du haut de la page
 function scrollFunction() {
 if (
 document.body.scrollTop > 20 ||
@@ -32,6 +36,7 @@ mybutton.style.display = "none";
 }
 }
 
+// Quand l'utilisateur clique sur le bouton, retour en haut
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
